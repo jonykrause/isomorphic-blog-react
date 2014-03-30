e9502b99d326bb6cdd64d23d1ec5d8f6ee7eb6b2
jonykrause <buero@jonathan-krause.de>
2014-03-30 20:43:40 +0200

    run test build

--- app/components/app.js
+++ app/components/app.js
@@ -32,7 +32,7 @@ var App = React.createClass({
           <link type="text/plain" rel="author" href="/humans.txt" />
           <link rel="icon" href="/img/favicon.ico" />
           <link href="/rss.xml" type="application/rss+xml" rel="alternate" />
-          <link rel="stylesheet" href="/css/style.css" />
+          <link rel="stylesheet" href="/css/bundle.css" />
           <script src="/js/bundle.js" />
         </head>
         <Pages className="page" path={this.props.path}>
@@ -52,8 +52,6 @@ var App = React.createClass({
 if (!isServer()) {
   window.onload = function() {
     React.renderComponent(App(), document);
-
-
     (function(a,e,f,g,b,c,d){a.GoogleAnalyticsObject=b;a[b]=a[b]||function(){(a[b].q=a[b].q||[]).push(arguments)};a[b].l=1*new Date;c=e.createElement(f);d=e.getElementsByTagName(f)[0];c.async=1;c.src=g;d.parentNode.insertBefore(c,d)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","UA-37971335-1","jonykrau.se");ga("send","pageview");
 
   }
